26d90148ac4a6124618e7e402e4337f39ab3a296
Antonio <antonio.mattesco01@gmail.com>
2025-01-09 19:22:41 +0100

    Update FINAL_CODE.R

--- Code/FINAL_CODE.R
+++ Code/FINAL_CODE.R
@@ -7,21 +7,24 @@ Upload only the sections of code that you are confident work correctly and are r
 ## Package Loading and General Configuration
 
 ```{r LIBRARIES}
+rm(list=ls())
 library(readxl)
 library(readr)
 ```
 
-```{r SETWD}
-setwd("D:/Projects_GitHub/BEFD_Project")
+```{r SETWD, warning=FALSE}
+# Set base path
+base_path <- "D:/Projects_GitHub/BEFD_Project/Data"
 ```
 
 ## Data Loading and Preprocessing
 
-```{r DATA LOAD}
-data <- read_csv("Data/data.csv")
-data$Date <- as.Date(data$Date, format = "%Y-%m-%d") # ensure dt format
+```{r DATA LOAD, message=FALSE, warning=FALSE}
+data <- read_csv(file.path(base_path, "data.csv"))
+data$Date <- as.Date(data$Date, format = "%Y-%m-%d") # ensure date format
 data <- data %>% mutate(Month = format(Date, "%m"),
                         Year = as.factor(format(Date, "%Y"))) # create useful features for the models
+data$trend <- 1:nrow(data)
 head(data)
 ```
 
@@ -31,10 +34,8 @@ cat("NA counts in dataset:\n")
 cat(paste(names(data), colSums(is.na(data)), sep=": "), sep="\n")
 ```
 
-Then we load a dataset found on [EUMOFA](https://eumofa.eu/web/guest/bulk-download)
-```{r}
-
-```
+Then we load a dataset found on [EUMOFA](https://eumofa.eu/web/guest/bulk-download).
+Here we focus onthe salmon consumption only beacusa we have seen that other fishes or thei aggregate value lead worst results.
 
 
 
