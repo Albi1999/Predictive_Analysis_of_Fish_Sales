c9923d02dfbbc1d0f63aa27f0b3633ae13881c38
Antonio <antonio.mattesco01@gmail.com>
2025-01-10 17:56:42 +0100

    Update FINAL_CODE.R

--- Code/FINAL_CODE.R
+++ Code/FINAL_CODE.R
@@ -17,7 +17,6 @@ library(gridExtra)
 
 ```{r SETWD, warning=FALSE}
 base_path <- "D:/Projects_GitHub/BEFD_Project/" # Set base path
-source(file.path(base_path, "Code/Functions_Utilies.R"))
 ```
 
 ## Data Loading and Preprocessing
@@ -68,7 +67,7 @@ kg: Represents the quantity of salmon sold in Italy, measured in kilograms.
 
 Finally we aggregate the salmon monthly consumption time series to our data.
 ```{r ASSIGN KG_STD TO DATA}
-data$fish_cons <- fish_cons$kg_std
+data$fish_cons <- fish_cons$kg_std[,1]
 head(data,3)
 ```
 
@@ -140,32 +139,122 @@ In this section, we perform a train-test split to prepare the data for model tra
 
 ```{r TRAIN-TEST SPLIT}
 prop <- 0.8
-train_testm <- split_train_test(data, "Baccala_Mantecato", prop)
-plot_train_test(train_testm, "Baccala_Mantecato")
 
-train_testv <- split_train_test(data, "Baccala_Vicentina", prop)
-plot_train_test(train_testv, "Baccala_Vicentina")
+n_sample <- floor(nrow(data) * prop)
+  
+train <- data[1:n_sample, ]
+y_trainm <- train[["Baccala_Mantecato"]]
+  
+test <- data[(n_sample + 1):nrow(data), ]
+  
+y_testm <- test[["Baccala_Mantecato"]]
+y_testv <- test[["Baccala_Mantecato"]]
+```
 
-trainm = train_testm$train
-y_train_m = train_testm$y_train
-testm = train_testm$test
-y_test_m = train_testm$y_test
+```{r TRAIN-TEST PLOT}
+Type = c(rep("Train", dim(train)[1]), rep("Test", dim(test)[1]))
+ggplot(data, aes(x = trend, y = Baccala_Mantecato, color = Type)) +
+  geom_line() +
+  scale_color_manual(values = c("red", "blue")) +
+  labs(title = "Train and Test Data for Baccala Mantecato",
+       x = "Time",
+       y = "Quantity sold of Baccala Mantecato") +
+  theme_minimal() +
+  theme(legend.title = element_blank())
 
-trainv = train_testv$train
-y_train_v = train_testv$y_train
-testv = train_testv$test
-y_test_v = train_testv$y_test
+ggplot(data, aes(x = trend, y = Baccala_Vicentina, color = Type)) +
+  geom_line() +
+  scale_color_manual(values = c("red", "blue")) +
+  labs(title = "Train and Test Data for Baccala Vicentinao",
+       x = "Time",
+       y = "Quantity sold of Baccala Vicentina") +
+  theme_minimal() +
+  theme(legend.title = element_blank())
 ```
 
 ## Modelling Phase
 
+### Linear Regression Model
+
+We start by estimating a simple linear regression model with all the possible variables
+```{r}
+lr_m <- lm(Baccala_Mantecato ~ trend + Month + fish_cons, data = train)
+summary(lr_m)
+```
+
+The Multiple R-squared value of 0.9178 indicates that approximately 91.78% of the variability in Baccala_Mantecato is explained by the model.
+Then we take a closer look to the coefficents:
+- the trend variable has a positive coefficient of 0.18, meaning that, on average, Baccala_Mantecato increases by 0.18 units for each time period, assuming all other factors remain constant.
+-The coefficient for fish_cons is 7.79, indicating a strong positive relationship. Specifically, for each unit increase in fish consumption, Baccala_Mantecato rises by approximately 7.79 units. In particular we will provide an exemple: if the fish consumption in Italy increases by a unit in October 2021, then keeping all the other variables fixed, the quantity sold in December 2021 will increase of 7.79 kg.
+- Regarding the Month variables, they capture seasonal effects by representing deviations from the baseline month (January). Some months show statistically significant effects (e.g., February, August, September, October, and December. For example, December shows a substantial positive effect, indicating a peak in that month, while months like February and October have negative coefficients, reflecting significant declines compared to January.
+
+Now we try removing variables to evaluate their impact on model performance using AIC and adjusted R² values.
+
+```{r}
+lr_m_month <- lm(Baccala_Mantecato ~ trend + fish_cons, data = train)
+lr_m_trend <- lm(Baccala_Mantecato ~ Month + fish_cons, data = train)
+lr_m_fish_cons <- lm(Baccala_Mantecato ~ trend + Month, data = train)
+
+cat("Model with trend and fish consumption:\n")
+cat("  AIC:", AIC(lr_m_month), "\n")
+cat("  Adjusted R²:", summary(lr_m_month)$adj.r.squared, "\n\n")
+
+cat("Model with trend and month:\n")
+cat("  AIC:", AIC(lr_m_fish_cons), "\n")
+cat("  Adjusted R²:", summary(lr_m_fish_cons)$adj.r.squared, "\n\n")
+
+cat("Model with month and fish consumption:\n")
+cat("  AIC:", AIC(lr_m_trend), "\n")
+cat("  Adjusted R²:", summary(lr_m_trend)$adj.r.squared, "\n\n")
+
+cat("Full model:\n")
+cat("  AIC:", AIC(lr_m), "\n")
+cat("  Adjusted R²:", summary(lr_m)$adj.r.squared, "\n")
+```
+From the results above we clearly see that the best model fit belong to the full model with all predictors: trend, monthly seasonality and fish consumption.
+
+The same analysis is conducted below for the Baccala Vicentina variable.
+
+```{r}
+lr_v_full <- lm(Baccala_Vicentina ~ trend + Month + fish_cons, data = train)
+summary(lr_v_full)
+```
+
+This time the fish consumption seems to be not useful to imporve the model pefromance so we remove it
+
+```{r}
+lr_v <- update(lr_v_full, .~. - fish_cons)
+summary(lr_v)
+```
+The adjusted R² increses from 0.8261 to 0.8305 suggesting that the redced model is better than the full one. 
+Furthermore, the trend variable shows a large p-value, suggesting that it can be removed from the model without significantly affecting the results.
+
+```{r}
+lr_v <- update(lr_v, .~. - trend)
+summary(lr_v)
+```
+
+The best model for the Baccala Vicentina series is the one that includes only the monthly seasonality. This model provides the best fit, indicating that the variations in the data are primarily driven by seasonal effects, without the need for additional trend or other predictors.
+Moreover, the fitted model is statistically significant, as shown by the F-statistic of 18.21, with a p-value of 1.684e-09, confirming the model's overall significance.
+
+Below, we compare the difference in AIC and adjusted R² between the full model and the one with only the monthly seasonality.
+```{r}
+cat("Model with month:\n")
+cat("  AIC:", AIC(lr_v), "\n")
+cat("  Adjusted R²:", summary(lr_v)$adj.r.squared, "\n\n")
+
+cat("Full model:\n")
+cat("  AIC:", AIC(lr_v_full), "\n")
+cat("  Adjusted R²:", summary(lr_v_full)$adj.r.squared, "\n")
+```
+The model with only the monthly seasonality performs better than the full model in terms of both AIC and adjusted R².
 
 
+## ARIMA Model
 
+```{r}
 
-
-
-
+```
 
 
 
