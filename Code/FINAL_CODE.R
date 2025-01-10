3af5168400050bd8b6109693cc6126c4965ee842
Antonio <antonio.mattesco01@gmail.com>
2025-01-10 11:38:54 +0100

    Update FINAL_CODE.R

--- Code/FINAL_CODE.R
+++ Code/FINAL_CODE.R
@@ -6,15 +6,15 @@ Upload only the sections of code that you are confident work correctly and are r
 
 ## Package Loading and General Configuration
 
-```{r LIBRARIES}
+```{r LIBRARIES, message=FALSE, warning=FALSE}
 rm(list=ls())
 library(readxl)
 library(readr)
+library(dplyr)
 ```
 
 ```{r SETWD, warning=FALSE}
-# Set base path
-base_path <- "D:/Projects_GitHub/BEFD_Project/Data"
+base_path <- "D:/Projects_GitHub/BEFD_Project/Data" # Set base path
 ```
 
 ## Data Loading and Preprocessing
@@ -22,10 +22,10 @@ base_path <- "D:/Projects_GitHub/BEFD_Project/Data"
 ```{r DATA LOAD, message=FALSE, warning=FALSE}
 data <- read_csv(file.path(base_path, "data.csv"))
 data$Date <- as.Date(data$Date, format = "%Y-%m-%d") # ensure date format
-data <- data %>% mutate(Month = format(Date, "%m"),
-                        Year = as.factor(format(Date, "%Y"))) # create useful features for the models
+data <- data %>% mutate(Month = format(Date, "%m")) # create useful features for the models
 data$trend <- 1:nrow(data)
-head(data)
+
+head(data,4)
 ```
 
 Check for missing values
@@ -35,7 +35,7 @@ cat(paste(names(data), colSums(is.na(data)), sep=": "), sep="\n")
 ```
 
 Then we load a dataset found on [EUMOFA](https://eumofa.eu/web/guest/bulk-download).
-Here we focus onthe salmon consumption only beacusa we have seen that other fishes or thei aggregate value lead worst results.
+Here we focus on the salmon consumption only beacuse we have seen that other fishes or their aggregate value lead worst results.
 
 
 
